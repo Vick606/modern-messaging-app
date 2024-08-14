80ffdad1339d2ded11baef3a7fcbe34f2b6c158b
Victor Koech <vickoch20@gmail.com>
2024-08-14 12:56:19 +0300

    Add MongoDB connection

--- server/server.js
+++ server/server.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('MongoDB connected'))
+  .catch((err) => console.log('MongoDB connection error:', err));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
