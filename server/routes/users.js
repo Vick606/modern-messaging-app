327fd653b58267a62e074d28296afdc6114e49db
Victor Koech <vickoch20@gmail.com>
2024-08-14 16:04:28 +0300

    Add a new route for updating status

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { getProfile, updateProfile } = require('../controllers/userController');
 const auth = require('../middleware/auth');
+const { updateStatus } = require('../controllers/statusController');
 
 router.get('/profile', auth, getProfile);
 router.put('/profile', auth, updateProfile);
+router.put('/status', auth, updateStatus);
 
 module.exports = router;
