b5045d8d3332ca9a52fe8bf8afee0f3e13ab858a
Victor Koech <vickoch20@gmail.com>
2024-08-14 16:36:44 +0300

    Update to support group message

--- server/models/Message.js
+++ server/models/Message.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 const messageSchema = new mongoose.Schema({
   sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  recipients: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   content: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
   fileUrl: { type: String },
   fileName: { type: String },
+  group: { type: mongoose.Schema.Types.ObjectId, ref: 'Group' },
 });
 
 module.exports = mongoose.model('Message', messageSchema);
