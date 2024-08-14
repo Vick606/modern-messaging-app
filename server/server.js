d865705edb4c6eaa24644e05fb0985cbd546f44e
Victor Koech <vickoch20@gmail.com>
2024-08-14 13:50:33 +0300

    Update server to include new routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const messageRoutes = require('./routes/messages');
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
 
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
