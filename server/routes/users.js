29b6ff7376dc4d5c711d869e77557e37222f8b69
Victor Koech <vickoch20@gmail.com>
2024-08-15 07:28:18 +0300

    Create new routes for user profile and settings

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getProfile, updateProfile } = require('../controllers/userController');
+const { getProfile, updateProfile, getSettings, updateSettings } = require('../controllers/userController');
 const auth = require('../middleware/auth');
 const { updateStatus } = require('../controllers/statusController');
 
 router.get('/profile', auth, getProfile);
 router.put('/profile', auth, updateProfile);
 router.put('/status', auth, updateStatus);
+router.get('/settings', auth, getSettings);
+router.put('/settings', auth, updateSettings);
 
 module.exports = router;
