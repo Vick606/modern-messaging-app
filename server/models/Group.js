1f305450b25b002c2063bf1ed917b469a26ed20f
Victor Koech <vickoch20@gmail.com>
2024-08-15 07:56:15 +0300

    Enhance group model

--- server/models/Group.js
+++ server/models/Group.js
@@ -5,6 +5,7 @@ const groupSchema = new mongoose.Schema({
   members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
   createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   createdAt: { type: Date, default: Date.now },
+  lastMessage: { type: mongoose.Schema.Types.ObjectId, ref: 'Message' },
 });
 
 module.exports = mongoose.model('Group', groupSchema);
