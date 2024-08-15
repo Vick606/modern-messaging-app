b9d1ed33a8e98c3cafaa51306f63aa4f25b6610b
Victor Koech <vickoch20@gmail.com>
2024-08-15 06:50:13 +0300

    Implement message encryption

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,31 +1,41 @@
 const Message = require('../models/Message');
 const multer = require('multer');
 const path = require('path');
+const CryptoJS = require('crypto-js');
 
-exports.sendMessage = async (req, res) => {
+exports.sendMessage = [upload.single('file'), async (req, res) => {
   try {
     const { recipientId, content } = req.body;
+    const encryptedContent = CryptoJS.AES.encrypt(content, ENCRYPTION_KEY).toString();
     const message = new Message({
       sender: req.user.id,
-      recipient: recipientId,
-      content
+      recipients: [recipientId],
+      content: encryptedContent,
+      fileUrl: req.file ? `/uploads/${req.file.filename}` : null,
+      fileName: req.file ? req.file.originalname : null
     });
     await message.save();
     res.status(201).json(message);
   } catch (error) {
     res.status(500).json({ error: 'Failed to send message' });
   }
-};
+}];
 
 exports.getMessages = async (req, res) => {
   try {
     const messages = await Message.find({
       $or: [
-        { sender: req.user.id, recipient: req.params.recipientId },
-        { sender: req.params.recipientId, recipient: req.user.id }
+        { sender: req.user.id, recipients: req.params.recipientId },
+        { sender: req.params.recipientId, recipients: req.user.id }
       ]
     }).sort('timestamp');
-    res.json(messages);
+    
+    const decryptedMessages = messages.map(message => ({
+      ...message._doc,
+      content: CryptoJS.AES.decrypt(message.content, ENCRYPTION_KEY).toString(CryptoJS.enc.Utf8)
+    }));
+    
+    res.json(decryptedMessages);
   } catch (error) {
     res.status(500).json({ error: 'Failed to retrieve messages' });
   }
