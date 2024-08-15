d3d3594ee83ba4ee264af7c1e8decc0ba0e09bd4
Victor Koech <vickoch20@gmail.com>
2024-08-15 07:47:53 +0300

    Add new routes for friend management

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,5 +9,8 @@ router.put('/profile', auth, updateProfile);
 router.put('/status', auth, updateStatus);
 router.get('/settings', auth, getSettings);
 router.put('/settings', auth, updateSettings);
+router.post('/friends/:id', auth, addFriend);
+router.delete('/friends/:id', auth, removeFriend);
+router.get('/friends', auth, getFriends);
 
 module.exports = router;
