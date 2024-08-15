ba8fecb66ca613e4c6fcb4798a2a62f1c1949a3a
Victor Koech <vickoch20@gmail.com>
2024-08-15 14:17:36 +0300

    Add error handling for PersistGate

--- client/src/index.js
+++ client/src/index.js
@@ -10,9 +10,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-      </PersistGate>
+    <PersistGate 
+  loading={null} 
+  persistor={persistor}
+  onBeforeLift={() => {
+  }}
+>
+  <App />
+</PersistGate>
     </Provider>
   </React.StrictMode>
 );
